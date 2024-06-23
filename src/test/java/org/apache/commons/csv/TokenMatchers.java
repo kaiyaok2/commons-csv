d9bf887f39f3953e682fb508cbab14c65860675c
kaiyaok2 <kaiyaok2@illinois.edu>
2024-06-24 04:37:54 +0800

    Revert "first"
    This reverts commit 1c4484beaebbdd2f7d577f935e31a278a5f9e3ba.

--- src/test/java/org/apache/commons/csv/TokenMatchers.java
+++ src/test/java/org/apache/commons/csv/TokenMatchers.java
@@ -18,6 +18,9 @@ package org.apache.commons.csv;
 
 import static org.hamcrest.core.AllOf.allOf;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeDiagnosingMatcher;
 
 /**
  * Collection of matchers for asserting the type and content of tokens.
@@ -28,3 +31,59 @@ final class TokenMatchers {
         return new TypeSafeDiagnosingMatcher<Token>() {
 
             @Override
+            public void describeTo(final Description description) {
+                description.appendText("token has content ");
+                description.appendValue(expectedContent);
+            }
+
+            @Override
+            protected boolean matchesSafely(final Token item,
+                    final Description mismatchDescription) {
+                mismatchDescription.appendText("token content is ");
+                mismatchDescription.appendValue(item.content.toString());
+                return expectedContent.equals(item.content.toString());
+            }
+        };
+    }
+
+    public static Matcher<Token> hasType(final Token.Type expectedType) {
+        return new TypeSafeDiagnosingMatcher<Token>() {
+
+            @Override
+            public void describeTo(final Description description) {
+                description.appendText("token has type ");
+                description.appendValue(expectedType);
+            }
+
+            @Override
+            protected boolean matchesSafely(final Token item,
+                    final Description mismatchDescription) {
+                mismatchDescription.appendText("token type is ");
+                mismatchDescription.appendValue(item.type);
+                return item.type == expectedType;
+            }
+        };
+    }
+
+    public static Matcher<Token> isReady() {
+        return new TypeSafeDiagnosingMatcher<Token>() {
+
+            @Override
+            public void describeTo(final Description description) {
+                description.appendText("token is ready ");
+            }
+
+            @Override
+            protected boolean matchesSafely(final Token item,
+                    final Description mismatchDescription) {
+                mismatchDescription.appendText("token is not ready ");
+                return item.isReady;
+            }
+        };
+    }
+
+    public static Matcher<Token> matches(final Token.Type expectedType, final String expectedContent) {
+        return allOf(hasType(expectedType), hasContent(expectedContent));
+    }
+
+}
